a1f6f49f359a83efdb051e43a20e92a3ad9c0909
jean_franco <82102346+jean-franco660@users.noreply.github.com>
2024-06-22 08:33:59 -0500

    Delete gestion_asistencia directory

--- gestion_asistencia/lib/local/condb/conexiondb.dart
+++
@@ -1,6 +0,0 @@
-import 'package:asistencia_app/local/db/database.dart';
-mixin ConexionDBL{
-  Future<AppDatabase> conetion() async{
-    return await $FloorAppDatabase.databaseBuilder('app_eventos.db').build();
-  }
-}
